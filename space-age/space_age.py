301cbc99bd37bbefc4d81d12dfd53815af068e5b
Danylo Svatyuk <danylo.svatyuk@solarwinds.com>
2018-07-26 16:07:50 +0300

    Space age exercise

---
+++ space-age/space_age.py
@@ -0,0 +1,30 @@
+class SpaceAge(object):
+    def __init__(self, seconds):
+        self.seconds = seconds
+
+    def on_earth_not_rounded(self):
+        return self.seconds/(60*60*24*365.25)
+
+    def on_earth(self):
+        return round(self.on_earth_not_rounded(), 2)
+
+    def on_mercury(self):
+        return round(self.on_earth_not_rounded()/0.2408467, 2)
+
+    def on_venus(self):
+        return round(self.on_earth_not_rounded()/0.61519726, 2)
+
+    def on_mars(self):
+        return round(self.on_earth_not_rounded()/1.8808158, 2)
+
+    def on_jupiter(self):
+        return round(self.on_earth_not_rounded()/11.862615, 2)
+
+    def on_saturn(self):
+        return round(self.on_earth_not_rounded()/29.447498, 2)
+
+    def on_uranus(self):
+        return round(self.on_earth_not_rounded()/84.016846, 2)
+
+    def on_neptune(self):
+        return round(self.on_earth_not_rounded()/164.79132, 2)
